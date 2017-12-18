f4857e52f76fedb4b8b827555972bbcb50d88f8d
Programista2 <programista2@grupainteger.pl>
2017-12-18 09:47:49 +0100

    correct translate message

--- src/Component/Translate/TranslateDictionary.php
+++ src/Component/Translate/TranslateDictionary.php
@@ -10,19 +10,19 @@ namespace CSC\Component\Translate;
 class TranslateDictionary
 {
     const
-        KEY_ELEMENT_DOES_NOT_EXIST = 'csc.request.element_does_not_exist',
-        KEY_ELEMENT_UNSUPPORTED_PARAMETERS = 'csc.request.unsupported_parameters',
-        KEY_PARAMETER_DOES_NOT_EXIST = 'csc.request.parameter_does_not_exist',
-        KEY_VALIDATION_ERROR = 'csc.request.validation_error',
-        KEY_FORGOT_REQUIRED_PARAMETERS = 'csc.request.forgot_required_parameters',
-        KEY_BAD_OPERATOR_IN_FILTER = 'csc.request.bad_operator_in_filter',
-        KEY_BAD_PARAMETER = 'csc.request.bad_parameter',
-        KEY_INVALID_TEXT_PATTERN = 'csc.request.invalid_text_pattern',
-        KEY_ID_ALREADY_EXISTS = 'csc.id_already_exists',
-        KEY_MERGE_GENERAL_ERROR = 'csc.merge_general_error',
-        KEY_NOT_SUPPORTED_FILTER = 'csc.not_supported_filter',
-        KEY_NOT_SUPPORTED_SORT_PARAMETER = 'csc.not_supported_sort_parameter',
-        KEY_NOT_SUPPORTED_TYPE_OF_USER = 'csc.not_supported_type_of_user',
-        KEY_UNABLE_CLEAR_TOKEN = 'csc.unable_clear_token'
+        KEY_ELEMENT_DOES_NOT_EXIST = 'element_does_not_exist',
+        KEY_ELEMENT_UNSUPPORTED_PARAMETERS = 'unsupported_parameters',
+        KEY_PARAMETER_DOES_NOT_EXIST = 'parameter_does_not_exist',
+        KEY_VALIDATION_ERROR = 'validation_error',
+        KEY_FORGOT_REQUIRED_PARAMETERS = 'forgot_required_parameters',
+        KEY_BAD_OPERATOR_IN_FILTER = 'bad_operator_in_filter',
+        KEY_BAD_PARAMETER = 'bad_parameter',
+        KEY_INVALID_TEXT_PATTERN = 'invalid_text_pattern',
+        KEY_ID_ALREADY_EXISTS = 'id_already_exists',
+        KEY_MERGE_GENERAL_ERROR = 'merge_general_error',
+        KEY_NOT_SUPPORTED_FILTER = 'not_supported_filter',
+        KEY_NOT_SUPPORTED_SORT_PARAMETER = 'not_supported_sort_parameter',
+        KEY_NOT_SUPPORTED_TYPE_OF_USER = 'not_supported_type_of_user',
+        KEY_UNABLE_CLEAR_TOKEN = 'unable_clear_token'
     ;
 }
